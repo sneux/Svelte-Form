f9331974051824d358622ac96d4227f495365ef8
sneux <shunshynne@gmail.com>
2022-01-30 10:28:04 -0500

    first

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte'
+	let feedback = [
+		{
+			id: 1, 
+			rating: 10, 
+			text: 'this is some text'
+		},
+		{
+			id: 2, 
+			rating: 9, 
+			text: 'this is some text'
+		},
+		{
+			id: 3, 
+			rating: 8, 
+			text: 'this is some text'
+		}
+	]
+
+
+</script>
+
+<main class="container">
+	<FeedbackList feedback={feedback} />
+</main>
+
---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Card from './Card.svelte'
+    export let item 
+</script>
+
+<Card>
+    <div class="num-display">
+        {item.rating}
+    </div>
+    <div class="close">X</div>
+    <div class="text-display">
+        {item.text}
+    </div>
+</Card>
+
+<style>
+    .num-display {
+        position: absolute;
+        top: -10px;
+        left: -10px;
+        width: 50px;
+        height: 50px;
+        background: #ff6a95;
+        color: #fff;
+        border: 1px #eee solid;
+        border-radius: 50%;
+        padding: 10px;
+        text-align: center;
+        font-size: 19px;
+    }
+</style>
